93adf0c69dd1c49240c6169f21e83d5ef5778bf2
Christian Kjær <ckl@codetalk.io>
2024-06-14 15:34:21 +0200

    Revert "Set package to v14"
    This reverts commit b68921559429c8b8511a034851196a96a2758ad5.

--- Package.swift
+++ Package.swift
@@ -6,15 +6,14 @@ import PackageDescription
 let package = Package(
     name: "AlertToast",
     platforms: [
-        .iOS(.v14),
-        .macOS(.v11),
+        .iOS(.v15),
+        .macOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AlertToast",
-            targets: ["AlertToast"]
-        ),
+            targets: ["AlertToast"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -25,11 +24,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AlertToast",
-            dependencies: []
-        ),
+            dependencies: []),
         .testTarget(
             name: "AlertToastTests",
-            dependencies: ["AlertToast"]
-        ),
+            dependencies: ["AlertToast"]),
     ]
 )
